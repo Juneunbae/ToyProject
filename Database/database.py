874fde64d52dbb5cc2827b12338918f681b1b247
전은배 <dmsqo1403@naver.com>
2023-04-02 10:57:34 +0900

    Update database.py

--- Database/database.py
+++ Database/database.py
@@ -2,11 +2,6 @@ import pymysql
 
 def Connect() :
     conn = pymysql.connect(
-        host='3.37.57.131',
-        user = 'anasa',
-        password='anasa@1',
-        db='CHECKBOX',
-        port=3306,
-        charset='utf8'
+
     )
     return conn
